695159ad33d6db0dc27dbca68d2e48e47f03b676
TakWolf <takwolf@foxmail.com>
2015-01-26 00:37:05 +0800

    example page

--- js/include.js
+++ js/include.js
@@ -1,10 +1,23 @@
+/**
+ * create an iframe and load target url at hash
+ * like 'http://nyandoge.takwolf.com#http://blog.takwolf.com'
+ */
 (function() {
 
     var include = document.createElement("iframe");
-    include.style.position = "absolute";
+    include.style.position = "absolute"; /* set absolute will not affect layout */
     include.frameBorder = 0;
-    document.body.appendChild(include);
 
+    /* make sure that iframe is the first element in body */
+    window.addEventListener("load", function() {
+        if (document.body.hasChildNodes()) {
+            document.body.insertBefore(include, document.body.firstChild);
+        } else {
+            document.body.appendChild(include);
+        }
+    }, false);
+
+    /* adjust the iframe size equal to the window size */
     function adapterIFrame() {
         include.style.width = window.innerWidth + "px";
         include.style.height = window.innerHeight + "px";
@@ -12,6 +25,7 @@
     adapterIFrame();
     window.addEventListener("resize", adapterIFrame, false);
 
+    /* listen to the hash url change to load the page */
     function loadUrl() {
         include.src = window.location.hash.substr(1);
     }
